1c3a67761a5094ed51c33b0964743cd0d052b999
Sarah Rigg <bulbasarah118@gmail.com>
2024-10-24 11:22:11 -0400

    Merged Sarah and Bella Work

--- Scripts/Intro.cs
+++ Scripts/Intro.cs
@@ -2,39 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+//using static UnityEditor.Experimental.GraphView.Port;
 
 public class Intro : MonoBehaviour
 {
-    public TMP_Text gameBy;
-    public TMP_Text TheStruggle;
-    public TMP_Text names;
-    public TMP_Text controllerRec;
-    public TMP_Text buffer;
-    public int textInt = -1;
+    //public TMP_Text gameBy;
+    //public TMP_Text TheStruggle;
+    //public TMP_Text names;
+    //public TMP_Text controllerRec;
+    public TMP_Text screenText;
+    public string[] allText;
+    public Image[] allImages;
+    public Image introImage;
+    private int textInt = 0;
+    private int imageInt = 0;
     // Start is called before the first frame update
     void Start()
     {
-        gameBy.color = new Color32(255, 255, 255, 0); //0 in alpha sets them to transparent
-        TheStruggle.color = new Color32(255, 255, 255, 0);
-        names.color = new Color32(255, 255, 255, 0);
-        controllerRec.color = new Color32(255, 255, 255, 0);
-        // gameBy.faceColor = new Color32(255, 255, 255, 0);
-        int nameOrder = Random.Range(0, 3);
-        switch (nameOrder) //for fairness
-        {
-            case 0:
-                names.text = "aka<br>Sarah Rigg,<br>Bella Donatelli,<br>and Liana Bourdon"; //<br> is line break
-                break;
-            case 1:
-                names.text = "aka<br>Bella Donatelli,<br>Liana Bourdon,<br>and Sarah Rigg";
-                break;
-            case 2:
-                names.text = "aka<br>Liana Bourdon,<br>Sarah Rigg,<br>and Bella Donatelli";
-                break;
-        }
-        
-        StartCoroutine(fadeIn(gameBy)); //starts fading in text
+        screenText.color = new Color32(255, 255, 255, 0);
+        introImage.color = new Color32(255, 255, 255, 0);
+        screenText.text = allText[0];
+        introImage = allImages[0];
+        StartCoroutine(fadeIn(screenText)); //starts fading in text
+        StartCoroutine(fadeInImage(introImage));
     }
 
     // Update is called once per frame
@@ -51,26 +43,79 @@ public class Intro : MonoBehaviour
             currText.color = new Color32(255, 255, 255, System.Convert.ToByte(opacity)); //sets text to be more and more opaque 
             yield return null;
         }
-        textInt++;
 
-        switch (textInt) //switches to next text to fade in
-        {
-            case 0:
-                StartCoroutine(fadeIn(TheStruggle));
-                break;
-            case 1:
-                StartCoroutine(fadeIn(names));
-                break;
-            case 2:
-                StartCoroutine(fadeIn(controllerRec));
-                break;
-            case 3:
-                StartCoroutine(fadeIn(buffer));
-                break;
-            case 4:
-                SceneManager.LoadScene("Title"); //load title screen
-                break;
+        for (int buffer = 0; buffer < 150; buffer++) {
+            yield return null;
         }
+        //textInt++;
+        if (textInt == 4)
+        {
+            //SceneManager.LoadScene("Title");
+        }
+        StartCoroutine(fadeOut(screenText));
+        StartCoroutine(fadeOutImage(introImage));
+
     }
 
+    IEnumerator fadeOut(TMP_Text currText) //fade in, passes in current text mesh pro
+    {
+        //currText.text = "Hello";
+        for (int opacity = 255; opacity >= 0; opacity -= 3) //opacity is from 0 to 255
+        {
+            currText.color = new Color32(255, 255, 255, System.Convert.ToByte(opacity)); //sets text to be more and more opaque 
+            yield return null;
+        }
+
+        for (int buffer = 0; buffer < 60; buffer++)
+        {
+            yield return null;
+        }
+        textInt++;
+        if (textInt != 5)
+        {
+            screenText.text = allText[textInt];
+            StartCoroutine(fadeIn(screenText));
+            StartCoroutine(fadeInImage(introImage));
+        }
+        else
+        {
+            SceneManager.LoadScene("Title");
+        }
+        
+    }
+
+
+
+
+    IEnumerator fadeInImage(Image currImage) //fade in, passes in current text mesh pro
+    {
+        //currText.text = "Hello";
+        for (int imopacity = 0; imopacity <= 255; imopacity += 5) //opacity is from 0 to 255
+        {
+            currImage.color = new Color32(255, 255, 255, System.Convert.ToByte(imopacity)); //sets text to be more and more opaque 
+            yield return null;
+        }
+
+        
+    }
+
+    IEnumerator fadeOutImage(Image currImage) //fade in, passes in current text mesh pro
+    {
+        //currText.text = "Hello";
+        for (int imopacity = 255; imopacity >= 0; imopacity -= 4) //opacity is from 0 to 255
+        {
+            currImage.color = new Color32(255, 255, 255, System.Convert.ToByte(imopacity)); //sets text to be more and more opaque 
+            yield return null;
+        }
+        currImage.color = new Color32(255, 255, 255, 0);
+
+        imageInt++;
+        if (imageInt != 5)
+        {
+            introImage = allImages[imageInt];
+            //screenText.text = allText[imageInt];
+            //StartCoroutine(fadeIn(screenText));
+        }
+
+    }
 }
